77dfcc77647aaacdd45aa32ce42288affd208cc6
deadman96385 <seanhoyt963@gmail.com>
2026-08-31 00:07:21 -0500

    libcamera: simple: skip SoftISP for direct sensors
    Signed-off-by: deadman96385 <seanhoyt963@gmail.com>

--- src/libcamera/pipeline/simple/simple.cpp
+++ src/libcamera/pipeline/simple/simple.cpp
@@ -620,7 +620,11 @@ int SimpleCameraData::init()
 	/*
 	 * Instantiate Soft ISP if this is enabled for the given driver and no converter is used.
 	 */
-	if (!converter_ && pipe->swIspEnabled()) {
+	const ControlInfoMap &sensorControls = sensor_->controls();
+	const bool supportsSwIspControls =
+		sensorControls.find(V4L2_CID_EXPOSURE) != sensorControls.end() &&
+		sensorControls.find(V4L2_CID_ANALOGUE_GAIN) != sensorControls.end();
+	if (!converter_ && pipe->swIspEnabled() && supportsSwIspControls) {
 		swIsp_ = std::make_unique<SoftwareIsp>(pipe, sensor_.get(), &controlInfo_);
 		if (!swIsp_->isValid()) {
 			LOG(SimplePipeline, Warning)
